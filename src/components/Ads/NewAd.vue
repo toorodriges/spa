81f3a1fecbefaf61696ca0afddae1152d8a2543f
toorodriges <toorodriges@gmail.com>
2019-05-30 00:43:59 +0300

    #21 Create ads in Firebase

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -4,27 +4,27 @@
 			<v-flex xs12 sm6 offset-sm3>
 				<h1 class="text--secondary mb-3">Create New Ad</h1>
 				<v-form
-						class="mb-3"
-						ref="form"
-						v-model="valid"
-						validation
+					class="mb-3"
+					ref="form"
+					v-model="valid"
+					validation
 				>
 					<v-text-field
-							:rules="[v => !!v || 'Title is required']"
-							label="Ad title"
-							name="title"
-							required
-							type="text"
-							v-model="title"
+						:rules="[v => !!v || 'Title is required']"
+						label="Ad title"
+						name="title"
+						required
+						type="text"
+						v-model="title"
 					></v-text-field>
 
 					<v-textarea
-							name="description"
-							label="Ad description"
-							type="text"
-							v-model="description"
-							multi-line
-							:rules="[v => !!v || 'Title is required']"
+						name="description"
+						label="Ad description"
+						type="text"
+						v-model="description"
+						multi-line
+						:rules="[v => !!v || 'Title is required']"
 					></v-textarea>
 				</v-form>
 				<v-layout row class="mb-3">
@@ -43,8 +43,8 @@
 				<v-layout row>
 					<v-flex xs12>
 						<v-switch
-								v-model="promo"
-								label="Ad to promo"
+							v-model="promo"
+							label="Ad to promo"
 						></v-switch>
 					</v-flex>
 				</v-layout>
@@ -52,9 +52,10 @@
 					<v-flex xs12>
 						<v-spacer></v-spacer>
 						<v-btn
-								:disabled="!valid"
-								@click="createAd"
-								class="success">
+							:loading="loading"
+							:disabled="!valid || loding"
+							@click="createAd"
+							class="success">
 							Create ad
 						</v-btn>
 					</v-flex>
@@ -64,28 +65,39 @@
 	</v-container>
 </template>
 <script>
-    export default {
-        data: function () {
-            return {
-                title: '',
-                description: '',
-                promo: false,
-                valid: false
-            }
-        },
-        methods: {
-            createAd() {
-                if (this.$refs.form.validate()) {
-                    //Logic
-                    const ad = {
-                        title: this.title,
-                        description: this.description,
-                        promo: this.promo,
-												imageSrc: 'https://webformyself.com/hivideo/images/js/vue.jpg'
-                    }
-                    this.$store.dispatch('createAd',ad)
-                }
-            }
+  export default {
+    data: function () {
+      return {
+        title: '',
+        description: '',
+        promo: false,
+        valid: false
+      }
+    },
+    computed: {
+      loading () {
+        return this.$store.getters.loading
+			}
+		},
+    methods: {
+      createAd() {
+        if (this.$refs.form.validate()) {
+          //Logic
+          const ad = {
+            title: this.title,
+            description: this.description,
+            promo: this.promo,
+            imageSrc: 'https://webformyself.com/hivideo/images/js/vue.jpg'
+          }
+          this.$store.dispatch('createAd', ad)
+						.then( () => {
+						  this.$router.push('/list')
+						})
+						.catch(() => {
+
+						})
         }
+      }
     }
+  }
 </script>
--- src/store/ads.js
+++ src/store/ads.js
@@ -1,3 +1,15 @@
+import  * as fb from 'firebase';
+class Ad {
+  constructor (title, description, ownerId, imageSrc = '', promo = false, id = null) {
+    this.title = title
+    this.description = description
+    this.ownerId = ownerId
+    this.imageSrc = imageSrc
+    this.promo = promo
+    this.id = id
+  }
+}
+
 export default {
   state: {
     ads: [
@@ -30,9 +42,29 @@ export default {
     }
   },
   actions: {
-    createAd({commit}, payload) {
-      payload.id = Math.random().toString()
-      commit('createAd', payload)
+    createAd: async function ({commit, getters}, payload) {
+      commit('clearError');
+      commit('setLoading', true);
+
+      try {
+        const newAd = new Ad(payload.title,
+          payload.description,
+          getters.user.id,
+          payload.imageSrc,
+          payload.promo
+        );
+        const ad = await fb.database().ref('ads').push(newAd);
+
+        commit('setLoading', false);
+        commit('createAd', {
+          ...newAd,
+          id: ad.key
+        })
+      } catch (error) {
+        commit('setError', error.message);
+        commit('setLoading', false);
+        throw error
+      }
     }
   },
   getters: {
@@ -52,6 +84,5 @@ export default {
         return state.ads.find(ad => ad.id === adId)
       }
     }
-
   }
 }
